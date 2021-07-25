5ed640fb5db31a326406eadcb898120d1028db88
Masa <katsukatsu.masa.0522@gmail.com>
2021-07-26 08:40:27 +0900

    axiosを用いたHTTP通信の実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React, {useState} from 'react';
+import Button from './components/Button';
+import Resources from './components/Resources';
+import jsonplaceholder from './apis/jsonplaceholder';
+
 
 const App = () => {
+    const [resources, setResources] = useState([]);
+
+    const getPosts = async() => {
+        try {
+            const posts = await jsonplaceholder.get('/posts');
+            setResources(posts.data);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    const getAlbums = async() => {
+        try {
+            const albums = await jsonplaceholder.get('/albums');
+            setResources(albums.data);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return(
-        <div className='ui container' style={{marginTop:'20px'}}>
-            <Counter />
+        <div className='ui container' style={{marginTop:'30px'}}>
+            <Button onClick={getPosts} color='primary' text='posts'/>
+            <Button onClick={getAlbums} color='red' text='albums'/>
+            <Resources resource={resources}/>
         </div>
     );
+
+    
 };
 
 export default App;
